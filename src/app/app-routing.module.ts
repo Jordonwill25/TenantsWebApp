465ad9e62f25d789dfbb1975d088a5ef65ed1131
Jordon Williams <101999495+Jordonwill25@users.noreply.github.com>
2023-08-16 09:25:16 -0500

    adding register, login and index page not complete tho

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login/login-page/login-page.component';
+import { RegisterComponent } from './login/register/register.component';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: IndexComponent},
+  {path:'login', component: LoginPageComponent},
+  {path:'register', component: RegisterComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login/login-page/login-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RegisterComponent } from './login/register/register.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    NavBarComponent,
+    RegisterComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
